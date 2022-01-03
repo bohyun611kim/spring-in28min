faa8d730876b0f4b0ace688df3e99f44ad9df4ab
tomdanbi <bohyun611.kim@gmail.com>
2022-01-03 10:53:33 +0900

    프로젝트 기본 완성 여기서부터 할것

--- src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,15 +1,15 @@
 package com.in28minutes.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
-@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
 
   private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -24,6 +24,16 @@ public class BinarySearchImpl {
     System.out.println(sortAlgorithm);
     // Search the array
     return 3;
+  }
+
+  @PostConstruct
+  public void postConstruct() {
+    logger.info("postConstruct");
+  }
+
+  @PreDestroy
+  public void preDestroy() {
+    logger.info("preDestroy");
   }
 
 }
